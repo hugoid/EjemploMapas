8e981dad776a1ab9e6b053e1908b055079faf01c
Hugo Izquierdo <hugo.izquierdo@colaboradoresunidadeditorial.es>
2016-11-24 07:47:05 +0100

    primer commit

---
+++ EjemploMapas/EjemploMapas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EjemploMapas
+//
+//  Created by Hugh on 24/11/16.
+//  Copyright © 2016 Hugo Izquierdo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
